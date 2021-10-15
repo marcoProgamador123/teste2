57331e3d0b0828a19c1265b616622866c682e65e
marcoProgamador123 <marco.ribeiro.andrade@escola.pr.gov.br>
2021-10-15 10:38:10 -0300

    add platform

--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -17,7 +19,7 @@ function setup(){
 
 
     ground = new Ground(600,height,1200,20);
-    platform = new Ground(150, 305, 300, 170);
+    platform = new Ground(150, 305, 300, 300);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -33,8 +35,10 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
+    log6 = new Log(230,180,80,PI/2);
 
     bird = new Bird(100,100);
+    chain = new Chain(bird.body,log6.body);
 
 }
 
@@ -58,7 +62,9 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
+    log6.display();
 
     bird.display();
     platform.display();
+    chain.display();
 }
